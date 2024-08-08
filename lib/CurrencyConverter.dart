d6d5b94255f1322376f9b03396e7c21018a5a2ab
angie elsegeiny <elsegeinyangie@gmail.com>
2024-08-08 20:37:37 +0300

    finished program

--- lib/CurrencyConverter.dart
+++ lib/CurrencyConverter.dart
@@ -1,7 +1,42 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class CurrencyConverter extends StatelessWidget {
+class CurrencyConverter extends StatefulWidget {
   const CurrencyConverter({super.key});
+
+  @override
+  State<CurrencyConverter> createState() => _CurrencyConverterStates();
+}
+
+class _CurrencyConverterStates extends State<CurrencyConverter> {
+  double result = 0.0;
+  final TextEditingController textEditingController = TextEditingController();
+
+  void convertButton() {
+    double? value = double.tryParse(textEditingController.text.trim());
+
+    if (value != null) {
+        result = value * 0.02;
+      setState(() {});
+    }
+  }
+
+  void resetButton() {
+    result = 0.0;
+    textEditingController.clear();
+    setState(() {
+    });
+  }
+
+  ButtonStyle buttonStyle() {
+    return const ButtonStyle(
+      backgroundColor:
+          WidgetStatePropertyAll(Color.fromARGB(255, 163, 217, 253)),
+      minimumSize: WidgetStatePropertyAll(Size(100, 50)),
+      foregroundColor: WidgetStatePropertyAll(Colors.black),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,22 +63,56 @@ class CurrencyConverter extends StatelessWidget {
           ],
         ),
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "00.00",
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 26,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  'USD $result',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40,
+                  ),
+                ),
               ),
-            ),
-            TextField(
-              
-            ),
-          ],
+              TextField(
+                controller: textEditingController,
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
+                decoration: const InputDecoration(
+                  filled: true,
+                  hintText: "Enter Amount in EGP",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(40)),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    TextButton(
+                      onPressed: convertButton,
+                      style: buttonStyle(),
+                      child: const Text("Convert"),
+                    ),
+                    TextButton(
+                      onPressed: resetButton,
+                      style: buttonStyle(),
+                      child: const Text("Reset"),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
